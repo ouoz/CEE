556fa6ff35bf4aef8b905d184b29ed22014a0144
ouoz <ouoz1V@gmail.com>
2020-06-25 08:50:33 +0900

    録音の追加

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from scipy.signal import argrelmax
 # interphone setting
 CHUNK = 1024
 RATE = 8000    # sampling rate
-dt = 1/RATE
+dt = 1.0/RATE
 freq = np.linspace(0,1.0/dt,CHUNK)
-fn = 1/dt/2    # nyquist freq
+fn = 1.0/dt/2    # nyquist freq
 FREQ_HIGH_BASE = 863.0  # high tone frequency
 FREQ_LOW_BASE = 691.0   # low tone frequency
 FREQ_ERR = 0.02         # allowable freq error
@@ -16,34 +16,26 @@ FREQ_ERR = 0.02         # allowable freq error
 detect_high = False
 detect_low = False
 
-# FFTで振幅最大の周波数を取得する関数
 def getMaxFreqFFT(sound, chunk, freq):
-    # FFT
     f = np.fft.fft(sound)/(chunk/2)
     f_abs = np.abs(f)
-    # ピーク検出
     peak_args = argrelmax(f_abs[:(int)(chunk/2)])
     f_peak = f_abs[peak_args]
     f_peak_argsort = f_peak.argsort()[::-1]
     peak_args_sort = peak_args[0][f_peak_argsort]
-    # 最大ピークをreturn
     return freq[peak_args_sort[0]]
 
-# 検知した周波数がインターホンの音の音か判定する関数
 def detectDualToneInOctave(freq_in, freq_high_base, freq_low_base, freq_err):
     det_h = det_l = False
-    # 検知した周波数が高音・低音のX倍音なのか調べる
     octave_h = freq_in / freq_high_base
     octave_l = freq_in / freq_low_base
     near_oct_h = round(octave_h)
     near_oct_l = round(octave_l)
     if near_oct_h == 0 or near_oct_l == 0:
         return False, False
-    # X倍音のXが整数からどれだけ離れているか
     err_h = np.abs((octave_h-near_oct_h) / near_oct_h)
     err_l = np.abs((octave_l-near_oct_l) / near_oct_l)
 
-    # 基音、２倍音、３倍音の付近であればインターホンの音とする
     if err_h < freq_err:
         det_h = True
     elif err_l < freq_err:
@@ -54,8 +46,8 @@ def detectDualToneInOctave(freq_in, freq_high_base, freq_low_base, freq_err):
 if __name__=='__main__':
     P = pyaudio.PyAudio()
     stream = P.open(format=pyaudio.paInt16, channels=1, rate=RATE, frames_per_buffer=CHUNK, input=True, output=False)
-    start = time.time() #高音が最後に記録された時間、デフォルトでは開始時間とする
-    cnt = 0 #高音がどれだけ続いたかを示す、この値によって低音の判定に移るか決める
+    start = time.time()
+    cnt = 0
 
     while stream.is_active():
         try:
@@ -64,13 +56,12 @@ if __name__=='__main__':
             abs_array = np.abs(ndarray)/32768
         
             if abs_array.max() > 0.001:
-                # FFTで最大振幅の周波数を取得
                 freq_max = getMaxFreqFFT(ndarray, CHUNK, freq)
                 print("Max Frequency:", freq_max, "Hz")
                 h,l = detectDualToneInOctave(freq_max, FREQ_HIGH_BASE, FREQ_LOW_BASE, FREQ_ERR)
                 cur  = time.time() 
                 if h:
-                    if cur - start > 1.0: #高音が記録された間隔が3秒以上以上開いていたら
+                    if cur - start > 1.0:
                         start = cur
                         cnt = 0
                     cnt += 1
--- test.py
+++ test.py
@@ -1,3 +1,111 @@
+import pyaudio
 import numpy as np
+import time
+from scipy.signal import argrelmax
 
-print("hello")
+# interphone setting
+CHUNK = 1024
+RATE = 8000    # sampling rate
+dt = 1/RATE
+freq = np.linspace(0,1.0/dt,CHUNK)
+fn = 1/dt/2    # nyquist freq
+FREQ_HIGH_BASE = 863.0  # high tone frequency
+FREQ_LOW_BASE = 691.0   # low tone frequency
+FREQ_ERR = 0.02         # allowable freq error
+#variable
+detect_high = False
+detect_low = False
+
+def getMaxFreqFFT(sound, chunk, freq):
+    f = np.fft.fft(sound)/(chunk/2)
+    f_abs = np.abs(f)
+    peak_args = argrelmax(f_abs[:(int)(chunk/2)])
+    f_peak = f_abs[peak_args]
+    f_peak_argsort = f_peak.argsort()[::-1]
+    peak_args_sort = peak_args[0][f_peak_argsort]
+    return freq[peak_args_sort[0]]
+
+def detectDualToneInOctave(freq_in, freq_high_base, freq_low_base, freq_err):
+    det_h = det_l = False
+    octave_h = freq_in / freq_high_base
+    octave_l = freq_in / freq_low_base
+    near_oct_h = round(octave_h)
+    near_oct_l = round(octave_l)
+    if near_oct_h == 0 or near_oct_l == 0:
+        return False, False
+    err_h = np.abs((octave_h-near_oct_h) / near_oct_h)
+    err_l = np.abs((octave_l-near_oct_l) / near_oct_l)
+    if err_h < freq_err:
+        det_h = True
+    elif err_l < freq_err:
+        det_l = True
+
+    return det_h, det_l
+
+if __name__=='__main__':
+    P = pyaudio.PyAudio()
+    stream = P.open(format=pyaudio.paInt16, channels=1, rate=RATE, frames_per_buffer=CHUNK, input=True, output=False)
+    start = time.time()
+    freq_max = -1000.0
+    cnt = 0
+    cur = 0
+    pos = 0
+    peek = []
+
+
+    while stream.is_active():
+        if len(peek) >= 2:
+            print(peeks)
+        try:
+            input = stream.read(CHUNK, exception_on_overflow=False)
+            ndarray = np.frombuffer(input, dtype='int16')
+            abs_array = np.abs(ndarray)/32768
+
+            if abs_array.max() > 0.01:
+                freq_max = getMaxFreqFFT(ndarray, CHUNK, freq)
+                if freq_max < 100.0:
+                    continue
+                print("Max Frequency:", freq_max, "Hz")
+            
+            if abs(pos - freq_max) < 0.001 :
+                pos = freq_max
+                cnt += 1
+            else:
+                cnt -= 1
+
+            if cnt > 5 and abs(freq_max - cur) > 0.001:   
+                cur = freq_max
+                peek.append(cur)
+                cnt = 0
+
+                """
+                h,l = detectDualToneInOctave(freq_max, FREQ_HIGH_BASE, FREQ_LOW_BASE, FREQ_ERR)
+                cur  = time.time() 
+                if h:
+                    if cur - start > 1.0:
+                        start = cur
+                        cnt = 0
+                    cnt += 1
+                    detect_high = True
+                    print("First Part Detected")
+                if l and cnt > 1:
+                    detect_low = True
+                    print("Second Part Detected")
+    
+                # dual tone detected
+                if detect_high and detect_low:
+                    print("Intercom has been detected !!!!!!!")
+                    time.sleep(5)
+                    print("flag reset")
+                    cnt = 0
+                    detect_high = detect_low = False
+                """
+    
+        except KeyboardInterrupt:
+            break
+
+
+
+    stream.stop_stream()
+    stream.close()
+    P.terminate()
