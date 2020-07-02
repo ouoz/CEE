f58fe788814889b2959f7de8de961871199649d4
ouoz <ouoz1v@gmail.com>
2020-07-02 17:39:36 +0900

    waon fixed

--- main.py
+++ main.py
@@ -9,12 +9,13 @@ RATE = 48000    # sampling rate
 dt = 1.0/RATE
 freq = np.linspace(0,1.0/dt,CHUNK)
 fn = 1.0/dt/2    # nyquist freq
-FREQ_HIGH_BASE = 863.0  # high tone frequency
-FREQ_LOW_BASE = 691.0   # low tone frequency
 FREQ_ERR = 0.02         # allowable freq error
+peeks = [float(val.strip()) for val in open("peeks.txt", 'r', encoding = "utf-8-sig").readlines()]
 #variable
-detect_high = False
-detect_low = False
+itr = 0
+det_cnt = 0
+
+print(peeks)
 
 def getMaxFreqFFT(sound, chunk, freq):
     f = np.fft.fft(sound)/(chunk/2)
@@ -23,31 +24,21 @@ def getMaxFreqFFT(sound, chunk, freq):
     f_peak = f_abs[peak_args]
     f_peak_argsort = f_peak.argsort()[::-1]
     peak_args_sort = peak_args[0][f_peak_argsort]
-    return freq[peak_args_sort[0]]
+    return [freq[peak_args_sort[0]], freq[peak_args_sort[1]], freq[peak_args_sort[2]], freq[peak_args_sort[0]], freq[peak_args_sort[0]]]
 
-def detectDualToneInOctave(freq_in, freq_high_base, freq_low_base, freq_err):
-    det_h = det_l = False
-    octave_h = freq_in / freq_high_base
-    octave_l = freq_in / freq_low_base
-    near_oct_h = round(octave_h)
-    near_oct_l = round(octave_l)
-    if near_oct_h == 0 or near_oct_l == 0:
-        return False, False
-    err_h = np.abs((octave_h-near_oct_h) / near_oct_h)
-    err_l = np.abs((octave_l-near_oct_l) / near_oct_l)
-
-    if err_h < freq_err:
-        det_h = True
-    elif err_l < freq_err:
-        det_l = True
-
-    return det_h, det_l
+def detectDualToneInOctave(freq_in, freq_high_base, freq_err):
+    for val in freq_in: 
+        if abs(val - freq_high_base) < freq_err: 
+            return True
+    
+    return False
 
 if __name__=='__main__':
     P = pyaudio.PyAudio()
     stream = P.open(format=pyaudio.paInt16, channels=1, rate=RATE, frames_per_buffer=CHUNK, input=True, output=False)
     start = time.time()
     cnt = 0
+    last_det = 0
 
     while stream.is_active():
         try:
@@ -57,27 +48,29 @@ if __name__=='__main__':
         
             if abs_array.max() > 0.01:
                 freq_max = getMaxFreqFFT(ndarray, CHUNK, freq)
-                print("Max Frequency:", freq_max, "Hz")
-                h,l = detectDualToneInOctave(freq_max, FREQ_HIGH_BASE, FREQ_LOW_BASE, FREQ_ERR)
-                cur  = time.time() 
-                if h:
-                    if cur - start > 1.0:
-                        start = cur
-                        cnt = 0
-                    cnt += 1
-                    detect_high = True
-                    print("First Part Detected")
-                elif l and cnt > 2:
-                    detect_low = True
-                    print("Second Part Detected")
-    
-                # dual tone detected
-                if detect_high and detect_low:
+                #print("Max Frequency:", freq_max, "Hz")
+                h = detectDualToneInOctave(freq_max, peeks[itr], FREQ_ERR)
+                cur_t  = time.time() 
+                
+                if cur_t - last_det > 1.0: 
+                    itr = 0
+                    cnt = 0
+
+                if cnt > 5 and itr == len(peeks) - 1: 
                     print("Intercom has been detected !!!!!!!")
+                    det_cnt += 1
                     time.sleep(5)
                     print("flag reset")
+                    itr = 0
                     cnt = 0
-                    detect_high = detect_low = False
+                elif cnt > 5 and itr != len(peeks) - 1 and detectDualToneInOctave(freq_max, peeks[itr + 1], FREQ_ERR): 
+                    itr += 1
+                    cnt = 0
+                    last_det = cur_t
+                    print("detected!!!")
+                elif h:
+                    cnt += 1
+                    last_det = cur_t
     
         except KeyboardInterrupt:
             break
--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from scipy.signal import argrelmax
 
 # interphone setting
 CHUNK = 1024
-RATE = 8000    # sampling rate
+RATE = 48000    # sampling rate
 dt = 1/RATE
 freq = np.linspace(0,1.0/dt,CHUNK)
 fn = 1/dt/2    # nyquist freq
@@ -15,7 +15,12 @@ FREQ_ERR = 0.02         # allowable freq error
 #variable
 detect_high = False
 detect_low = False
+streak = 5
 
+out_fn = "peeks.txt"
+
+
+# sampling highest frequency until 5th
 def getMaxFreqFFT(sound, chunk, freq):
     f = np.fft.fft(sound)/(chunk/2)
     f_abs = np.abs(f)
@@ -23,24 +28,8 @@ def getMaxFreqFFT(sound, chunk, freq):
     f_peak = f_abs[peak_args]
     f_peak_argsort = f_peak.argsort()[::-1]
     peak_args_sort = peak_args[0][f_peak_argsort]
-    return freq[peak_args_sort[0]]
+    return [freq[peak_args_sort[0]], freq[peak_args_sort[1]], freq[peak_args_sort[2]], freq[peak_args_sort[0]], freq[peak_args_sort[0]]] 
 
-def detectDualToneInOctave(freq_in, freq_high_base, freq_low_base, freq_err):
-    det_h = det_l = False
-    octave_h = freq_in / freq_high_base
-    octave_l = freq_in / freq_low_base
-    near_oct_h = round(octave_h)
-    near_oct_l = round(octave_l)
-    if near_oct_h == 0 or near_oct_l == 0:
-        return False, False
-    err_h = np.abs((octave_h-near_oct_h) / near_oct_h)
-    err_l = np.abs((octave_l-near_oct_l) / near_oct_l)
-    if err_h < freq_err:
-        det_h = True
-    elif err_l < freq_err:
-        det_l = True
-
-    return det_h, det_l
 
 if __name__=='__main__':
     P = pyaudio.PyAudio()
@@ -48,14 +37,12 @@ if __name__=='__main__':
     start = time.time()
     freq_max = -1000.0
     cnt = 0
-    cur = 0
     pos = 0
-    peek = []
+    peeks = []
+    cur = 0
 
 
     while stream.is_active():
-        if len(peek) >= 2:
-            print(peeks)
         try:
             input = stream.read(CHUNK, exception_on_overflow=False)
             ndarray = np.frombuffer(input, dtype='int16')
@@ -63,48 +50,37 @@ if __name__=='__main__':
 
             if abs_array.max() > 0.01:
                 freq_max = getMaxFreqFFT(ndarray, CHUNK, freq)
-                if freq_max < 100.0:
-                    continue
-                print("Max Frequency:", freq_max, "Hz")
-            
-            if abs(pos - freq_max) < 0.001 :
-                pos = freq_max
-                cnt += 1
-            else:
-                cnt -= 1
-
-            if cnt > 5 and abs(freq_max - cur) > 0.001:   
-                cur = freq_max
-                peek.append(cur)
-                cnt = 0
-
-                """
-                h,l = detectDualToneInOctave(freq_max, FREQ_HIGH_BASE, FREQ_LOW_BASE, FREQ_ERR)
-                cur  = time.time() 
-                if h:
-                    if cur - start > 1.0:
-                        start = cur
+                
+                if cnt > streak: 
+                    flag = False
+                    for val in freq_max: 
+                        if abs(val - cur) < 0.001: 
+                            flag = True
+                            break
+                    if not flag: 
+                        peeks.append(cur)
+                        cur = freq_max[0]
                         cnt = 0
-                    cnt += 1
-                    detect_high = True
-                    print("First Part Detected")
-                if l and cnt > 1:
-                    detect_low = True
-                    print("Second Part Detected")
-    
-                # dual tone detected
-                if detect_high and detect_low:
-                    print("Intercom has been detected !!!!!!!")
-                    time.sleep(5)
-                    print("flag reset")
+                        continue
+
+                flag = False
+                for val in freq_max:
+                    if abs(cur - val) < 0.001 :
+                        cnt += 1
+                        flag = True
+                        break
+                if flag: continue
+                else: 
                     cnt = 0
-                    detect_high = detect_low = False
-                """
+                    cur = freq_max[0]
     
         except KeyboardInterrupt:
             break
 
 
+    with open(out_fn, 'w', encoding = "utf-8-sig") as kaki: 
+        for val in peeks: 
+            kaki.write(str(val) + '\n')
 
     stream.stop_stream()
     stream.close()
