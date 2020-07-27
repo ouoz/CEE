13d9a52331d0f6ac03bdaf4baa41042775358d83
ouoz <ouoz1V@gmail.com>
2020-07-27 17:46:41 +0900

    server

---
+++ server.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Main.py
+from RN42 import RN42
+from time import sleep
+import RPi.GPIO as GPIO
+import sys
+import time
+
+class Main:
+    #""" Main Class """
+
+    # Setting & Connect
+    ras = RN42("ras", "9C:5C:F9:CD:A7:02", 1)
+    ras.connectBluetooth(ras.bdAddr,ras.port)
+
+    print("Entering main loop now")
+
+    while 1:
+        try:
+            ras.sock.send("Hello World")
+            print("data send")
+            time.sleep(1)
+        except KeyboardInterrupt:
+            ras.disconnect(ras.sock)
